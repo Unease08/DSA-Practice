34d17a0d902b7e0131c29f82712d90bda75fe2cd
Anish Shrestha <anish08shrestha@gmail.com>
2024-06-24 15:44:53 +0545

    solving contains duplicate problem

---
+++ 1. Contains Duplicate/Solution.cs
@@ -0,0 +1,13 @@
+﻿public class Solution
+{
+    public bool hasDuplicate(int[] nums)
+    {
+        HashSet<int> set = new HashSet<int> ();
+
+        foreach(int num in nums) {
+            if (set.Contains(num)) return true;
+            set.add(num);
+        }
+        return false
+    }
+}
